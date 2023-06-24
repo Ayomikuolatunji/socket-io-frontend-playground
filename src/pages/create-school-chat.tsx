660921b8d1b36a26ef3ad3065674813ad7dd5682
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-06-24 19:50:15 +0100

    chat playground

--- src/pages/create-school-chat.tsx
+++ src/pages/create-school-chat.tsx
@@ -1,54 +1,53 @@
-import React from "react";
-import {  useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
-const Index = () => {
+const ChatComponent = () => {
   const socketRef: any = useRef(null);
-
   useEffect(() => {
     socketRef.current = io("http://localhost:8080", {
-      query: { schoolId: "647d07ab514602c2d26f6c28" },
+      query: { schoolId: "64845984a54c67c196a1cd2c" },
     });
     socketRef.current.on("connect", () => {
       console.log("Connected to the server");
-      socketRef.current.emit("userLogin", "647d07ab514602c2d26f6c28");
+      socketRef.current.emit("userLogin", "64845984a54c67c196a1cd2c");
     });
-    socketRef.current.on("newChat", (data: any) => {
-      console.log("Received new event:", data);
+
+    socketRef.current.on("chatCreationError", (data: any) => {
+      console.log(data);
     });
-    socketRef.current.emit("fetchUserChats", {
-      userId: "647d07ab514602c2d26f6c28",
-      userType: "school",
-      userName:"sabis"
+    socketRef.current.emit("fetchChats", "64845984a54c67c196a1cd2c");
+    socketRef.current.on("chatsFetched", (data: any) => {
+      console.log(data);
     });
-    socketRef.current.on("userChats", (data: any) => {
+    socketRef.current.on("chatCreated", (data: any) => {
       console.log(data);
     });
     return () => {
-      socketRef.current.disconnect();
+      if (socketRef.current) {
+        socketRef.current.disconnect();
+      }
     };
   }, []);
 
-  const createEvent = () => {
-    const eventData = {
-      userId: "647d07ab514602c2d26f6c28",
-      userType: "school",
-      userName: "sabis",
-      user: {
-        name: "The user name of who they are chatting with either parent or teacher",
-        selectedUserId: "The Id of who they are chatting with either parent or teacher",
-        profilePicture: "The profile picture of who they are chatting with either parent or teacher",
-      },
+  const createChat = () => {
+    const newChat = {
+      senderId: "64845984a54c67c196a1cd2c",
+      receiverId: "6484c710d6c6006c9e2affc4",
+      senderType: "school",
+      receiverType: "parent",
+      sessionRef: "6484d41823096a8e15436e5e",
+      termRef: "6484d41823096a8e15436e60",
     };
-
-    socketRef.current.emit("createUserChat", eventData);
+    socketRef.current.emit("createChat", newChat);
   };
 
   return (
     <>
-      <button onClick={createEvent}>Create Event</button>
+      <div>
+        <button onClick={createChat}>Create a new chat</button>
+      </div>
     </>
   );
 };
 
-export default Index;
+export default ChatComponent;
